83d5c087b3adbc67fbfda4d9fc604bdc151ff4d9
Alex Cardozo <secundariaalex123@gmail.com>
2026-07-02 18:11:55 -0300

    Implementação do método POST, testes, e contrato via schemas.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,35 @@
 from http import HTTPStatus
 
+import pytest
 from fastapi.testclient import TestClient
 
 from fast_zero.app import app
 
 
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
+@pytest.fixture
+def client():
+    return TestClient(app)
 
+
+def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
